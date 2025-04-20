38a7809200544eff598aa2d14231f5bfe2474f2c
hmoon96 <hlmooney98@gmail.com>
2025-04-20 16:49:34 +0100

    Added the two views for the home and blog page. Also created a urls.py for the blog app

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
 
-def my_blog(request):
-    return HttpResponse("Hello, blog!")
+def home(request):
+    return render(request, 'blog/home.html')
+
+
+# Blog posts view
+def blog(request):
+    posts = Post.objects.all().order_by('-date_posted')
+    return render(request, 'blog/blog.html', {'posts': posts})
